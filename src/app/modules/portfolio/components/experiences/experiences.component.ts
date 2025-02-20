9e50fd2257e44f090a1e672a2c75c09d36822b08
Jose Messias <josecmessias22@gmail.com>
2025-02-20 14:36:47 -0300

    Adicionando atualização do Portfólio

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -14,17 +14,24 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
-        strong: 'Estágio Full-stack Developer',
-        p: 'DUO Technology | Abril 2024 - Outubro 2024',
+        strong: 'Assoc, Full-stack Developer',
+        p: 'Avanade Brasil | Dezembro 2024 - Present',
       },
-      text: '<p>De abril de 2024 a outubro de 2024, atuei como Estagiário na DUO Technology, destacando-me na área de Full-Stack Developer, utilizando HTML5, CSS3, JavaScript, TypeScript, Angular 2+, Node.js, Postman, PostgreSQL, MySQL e MongoDB.</p>',
+      text: '<p>Desde Dezembro de 2024, atuo como estariário na Avanade, destacando-me na área de Assoc, Full-stack Developer, utilizando Java, Spring Boot, Angular e SQL Server<p/>',
     },
     {
       summary: {
         strong: 'Trainee Software Developer',
-        p: 'DUO Technology | Outubro 2024 - Present',
+        p: 'DUO Technology | Outubro 2024 - Dezembro 2024',
       },
       text: '<p>Desde Outubro de 2024, atuo como Trainee na DUO Technology, destacando-me na área de Software Developer, utilizando Javascript, TypeScript, Angular, Node, Postman, PostgreSQL, Low-Code, No-Code e Mendix Studio Pro.<p/>',
     },
+    {
+      summary: {
+        strong: 'Estágio Full-stack Developer',
+        p: 'DUO Technology | Abril 2024 - Outubro 2024',
+      },
+      text: '<p>De abril de 2024 a outubro de 2024, atuei como Estagiário na DUO Technology, destacando-me na área de Full-Stack Developer, utilizando HTML5, CSS3, JavaScript, TypeScript, Angular 2+, Node.js, Postman, PostgreSQL, MySQL e MongoDB.</p>',
+    }
   ]);
 }
